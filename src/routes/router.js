c40351558b273140c3d7e9da4c604f2fb81430ac
Bahrom10 <bahodirovbahrom2010@gmail.com>
2024-08-26 13:27:45 +0500

    fdasfsa

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,9 +1,8 @@
 const get = (req, res) => {
     try {
-        res.send(req.query);
         const rawdata = fs.readFileSync("./data/messages.json");
         const messages = JSON.parse(rawdata);
-        res.send(messages);
+        res.send(messages, req.query);
     } catch (err) {
         console.error("Error reading messages.json:", err);
         res.status(500).send({ error: "Failed to read messages." });
